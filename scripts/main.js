423721402c84cc4353f376c78a202548065e2561
e-v-o-l-v-e <megninpreiss.ivanoe@gmail.com>
2024-05-15 12:32:23 +0200

    disabled ask name part

--- scripts/main.js
+++ scripts/main.js
@@ -12,23 +12,23 @@ myImage.onclick = () => {
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 
-function setUserName() {
-  const myName = prompt("Please enter your name.");
-  if (!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Bienvenue ${myName}`;
-  }
-}
+// function setUserName() {
+//   const myName = prompt("Please enter your name.");
+//   if (!myName) {
+//     setUserName();
+//   } else {
+//     localStorage.setItem("name", myName);
+//     myHeading.textContent = `Bienvenue ${myName}`;
+//   }
+// }
 
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Bienvenue ${storedName}`;
-};
+// if (!localStorage.getItem("name")) {
+//   setUserName();
+// } else {
+//   const storedName = localStorage.getItem("name");
+//   myHeading.textContent = `Bienvenue ${storedName}`;
+// };
 
-myButton.onclick = () => {
-  setUserName();
-}
+// myButton.onclick = () => {
+//   setUserName();
+// }
